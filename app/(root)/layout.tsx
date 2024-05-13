4d9df992ae837ebded121e15db017979b4a658aa
almustanir <olajidemubarak561@gmail.com>
2024-05-13 17:11:52 +0100

    right sidebar done___

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,16 +1,27 @@
+import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const loggedIn = {firstName: 'Mbtech', lastName: 'Dev'};
+  const loggedIn = { firstName: 'Mbtech', lastName: 'Dev' };
 
   return (
     <main className="flex h-screen w-full font-inter">
-       <Sidebar user={loggedIn}/>
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn}/>
+          </div>
+        </div>
         {children}
+      </div>
     </main>
   );
 }
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -27,14 +27,31 @@ const Sidebar = ({ user }: SiderbarProps) => {
 
                     return (
                         <Link href={item.route} key={item.label}
-                        className={cn('sidebar-link', {
-                            'bg-bank-gradient': isActive
-                        })}>
-                            {item.label}
+                            className={cn('sidebar-link', {
+                                'bg-bank-gradient': isActive
+                            })}>
+                            <div className='relative size-6'>
+                                <Image
+                                    src={item.imgURL}
+                                    alt={item.label}
+                                    fill
+                                    className={cn({
+                                        'brightness-[3] invert-0': isActive
+                                    })}
+                                />
+
+                            </div>
+                            <p className={cn('sidebar-label',{
+                                '!text-white' :isActive
+                            })}>
+                                {item.label}
+                                </p>
                         </Link>
                     )
                 })}
+                
             </nav>
+            FOOTER
         </section>
     )
 }
