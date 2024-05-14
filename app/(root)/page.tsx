8cb55f1a69229f95e29cec8093c96315eb56f3cd
almustanir <olajidemubarak561@gmail.com>
2024-05-14 12:56:00 +0100

    sidebar in progress

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
-    const loggedIn = {firstName: 'Mbtech'};
+    const loggedIn = {firstName: 'Mbtech', lastName: 'dev', email: 'dev@gmail.com'};
 
   return (
     <section className='home'>
@@ -21,7 +22,15 @@ const Home = () => {
             totalCurrentBalance={1250.35}
             />
         </header>
+
+
+        RECENT TRANSACTIONS
     </div>
+          <RightSidebar
+            user={loggedIn}
+            transactions={[]}
+            banks={[{ currentBalance: 123.50}, {currentBalance: 500.50}]}
+          />
     </section>
   )
 }
